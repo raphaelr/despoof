4cd5ed30d30fce75d2be3ccd32a049e660944812
Raphael Robatsch <raphaelr@f-m.fm>
2011-12-06 17:14:58 +0100

    version messup

--- include/despoof/win32/targetwindows.h
+++ include/despoof/win32/targetwindows.h
@@ -1,8 +1,8 @@
 #pragma once
 
-// Target Windows 2000
-#define _WIN32_WINNT 0x0500
-#define NTDDI_VERSION 0x05000000
+// Target Windows XP SP3
+#define _WIN32_WINNT 0x0501
+#define NTDDI_VERSION 0x05010300
 #include <sdkddkver.h>
 
 #define WIN32_LEAN_AND_MEAN
