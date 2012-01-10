4862c67f91ef5d0082280306f570518072430254
Raphael Robatsch <raphaelr@f-m.fm>
2012-01-10 14:02:27 +0100

    boost::format inserted an UTF16 string into an UTF8 destination

--- win32support/error.cpp
+++ win32support/error.cpp
@@ -1,8 +1,10 @@
 #include <boost/format.hpp>
+#include <boost/locale.hpp>
 #include <despoof/win32/targetwindows.h>
 #include <despoof/win32/error.h>
 
 using namespace boost;
+using namespace boost::locale::conv;
 
 void _throw_windows_error(const char *func, const char *file, unsigned line, DWORD error)
 {
@@ -13,5 +15,5 @@ void _throw_windows_error(const char *func, const char *file, unsigned line, DWO
 			throw windows_error("Fatal: FormatMessage failed");
 	}
 	
-	throw windows_error((format("%1% failed: %2% (From %3%#%4%) (Error code %5%)") % func % message % file % line % error).str());
+	throw windows_error((format("%1% failed: %2% (From %3%#%4%) (Error code %5%)") % func % utf_to_utf<char>(message) % file % line % error).str());
 }
